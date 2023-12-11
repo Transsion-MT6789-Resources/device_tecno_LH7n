5698e19a53866613c785eca982c675390159568d
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2023-12-12 02:02:54 +0700

    LH7n: Inherit configuration from common trees
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Include the common OEM chipset BoardConfig.
+include device/transsion/mt6789-common/BoardConfigCommon.mk
+
 DEVICE_PATH := device/tecno/LH7n
 
 # Inherit the proprietary files
--- device.mk
+++ device.mk
@@ -11,5 +11,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Inherit from the common OEM chipset makefile.
+$(call inherit-product, device/transsion/mt6789-common/mt6789.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/tecno/LH7n/LH7n-vendor.mk)
